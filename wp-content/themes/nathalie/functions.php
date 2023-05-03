9d9a9e5a862b28aa17dafca40a2c931c697f7826
kv6060 <seblanco60@hotmail.com>
2023-05-03 16:29:43 +0200

    thème personnalisé/ thème enfant

---
+++ wp-content/themes/nathalie/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+// Action qui permet de charger des scripts dans notre thème
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles(){
+    // Chargement du style.css du thème parent twenty twenty
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+ wp_enqueue_style( 'parent-style' , get_template_directory_uri() . '/style.css' );
+
+}
