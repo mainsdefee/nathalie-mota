58d8a039ed1205da15be970ff4b2fcf7e86816b2
kv6060 <seblanco60@hotmail.com>
2023-05-04 15:45:47 +0200

    média polices menus de navigation

--- wp-content/themes/nathalie/functions.php
+++ wp-content/themes/nathalie/functions.php
@@ -7,5 +7,12 @@ function theme_enqueue_styles(){
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
  wp_enqueue_style( 'parent-style' , get_template_directory_uri() . '/style.css' );
+ // Chargement du css/theme.css pour nos personnalisations
+   wp_enqueue_style( 'theme-style' , get_stylesheet_directory_uri() . '/css/theme.css' , array (), filemtime(get_stylesheet_directory() . '/css/theme.css' ));
 
 }
+/*charger les polices Google Fonts sur notre site WordPress*/
+function my_theme_enqueue_scripts() {
+  wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Space+Mono|Poppins&display=swap' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_scripts' );
